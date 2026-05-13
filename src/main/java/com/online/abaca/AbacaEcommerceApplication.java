296e5baa0f3fb83fe2cda5116a485440da85101a
Caelira <olaguera.prince.laranjo.ii@gmail.com>
2026-05-13 15:12:57 +0800

    init commit

---
+++ src/main/java/com/online/abaca/AbacaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.online.abaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacaEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacaEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online/abaca/AbacaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.abaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
